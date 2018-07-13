3244c20286dcb020c24fc948a3c408a6bf03b705
Siraj Raval <sirajravel@gmail.com>
2018-07-13 08:36:42 -0700

    Update 2b_cnn.js

--- public/scripts/2b_cnn.js
+++ public/scripts/2b_cnn.js
@@ -1,17 +1,27 @@
-// ***********************************
-// *                                 *
-// *  X-Team TensorFlow.js - CNN     *
-// *                                 *
-// ***********************************
 
-/**
- *  TensorFlow.js CNN
- */
-
+// Step 1 - build Convolutional network
 const buildCnn = function (data) {
+  //A promise represents the eventual result of an asynchronous 
+  //operation. It is a placeholder into which the successful 
+  //result value or reason for failure will materialize.
   return new Promise(function (resolve, reject) {
+    //Linear stack of layers.
     const model = tf.sequential()
 
+    //This layer creates a convolution kernel 
+    // that is convolved (actually cross-correlated) 
+    // with the layer input to produce a tensor of outputs. 
+
+  //kernel size - An integer or tuple/list of a single integer, 
+  // specifying the length of the 1D convolution window.
+  //filters - Integer, the dimensionality of the output space 
+  // (i.e. the number of filters in the convolution).
+  //stride- An integer or tuple/list of a single integer, 
+  // specifying the stride length of the convolution. 
+  //activation- nonlinearity
+  // kernel-init - An initializer for the bias vector.
+  //variance scaling - the weights initialization technique that tries to make the variance of the outputs 
+  //of a layer to be equal to the variance of its inputs
     model.add(tf.layers.conv1d({
       inputShape: [data.dates.length, 1],
       kernelSize: 100,
@@ -21,6 +31,10 @@ const buildCnn = function (data) {
       kernelInitializer: 'VarianceScaling'
     }))
 
+//poolsize - An integer or tuple/list of a single integer, 
+//representing the size of the pooling window.
+//strides: An integer or tuple/list of a single integer, 
+//specifying the strides of the pooling operation.
     model.add(tf.layers.maxPooling1d({
       poolSize: [500],
       strides: [2]
@@ -39,14 +53,16 @@ const buildCnn = function (data) {
       strides: [2]
     }))
 
-    //model.add(tf.layers.flatten({inputShape: [7, 7, 256]}))
-
+//dense (also known as a fully connected layer), 
+//which will perform the final classification. 
+// Flattening the output of a convolution+pooling layer pair before a dense layer is another common pattern in neural networks:
     model.add(tf.layers.dense({
       units: 10,
       kernelInitializer: 'VarianceScaling',
       activation: 'softmax'
     }))
-
+//The Promise.resolve(value) method returns a 
+//Promise object that is resolved with the given value. 
     return resolve({
       'model': model,
       'data': data
@@ -54,12 +70,8 @@ const buildCnn = function (data) {
   })
 }
 
-/**
- * TensorFlow.js
- *
- * See: https://medium.com/tensorflow/a-gentle-introduction-to-tensorflow-js-dba2e5257702
- */
 
+//Step 2 Train Model
 const cnn = function (model, data, cycles) {
   const tdates = tf.tensor1d(data.dates),
     thighs = tf.tensor1d(data.highs),
@@ -78,7 +90,6 @@ const cnn = function (model, data, cycles) {
     setTimeout(function () {
       try {
         model.compile({optimizer: 'sgd', loss: 'binaryCrossentropy', lr: 0.1})
-        /**
          model.fit(
          tdates.reshape([1, 1960, 1]),
          thighs.reshape([1, 1960, 1]), {
@@ -91,7 +102,6 @@ const cnn = function (model, data, cycles) {
           print(d.test_highs)
           resolve(print(''))
         })
-         */
       } catch (ex) {
         resolve(print(ex))
       }
@@ -99,10 +109,7 @@ const cnn = function (model, data, cycles) {
   })
 }
 
-/**
- * Init
- */
-
+// Step 3 - Execute! 
 print('Beginning AAPL CNN tests at ' + new Date() + '... this may take a while!')
 fetchWrapper('http://localhost:5555/api/').then(function (data) {
   prep(data).then(function (result) {
